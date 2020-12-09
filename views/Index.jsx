4f7394c9ece5118382fd3d3573f5e7c1b65aa474
Ben Ritter <Ben@Bens-MacBook-Pro.local>
2020-12-09 12:40:31 -0500

    testing erokuuuuuuu

--- views/Index.jsx
+++ views/Index.jsx
@@ -4,17 +4,17 @@ const Layout = require('./components/Layout.jsx');
 class Index extends React.Component {
     render() {
         return (
-            <Layout title="Index Page"
-                stylesheet="/css/style.css"
-                // js="index.js"
-            >
+            // <Layout title="Index Page"
+            //     stylesheet="/css/style.css"
+            //     // js="index.js"
+            // >
                 <h1>Index Page</h1>
                 {/* <ul className="index-page">
                     {
                         
                     }
                 </ul> */}
-            </Layout>
+            // </Layout>
         )
     }
 }
